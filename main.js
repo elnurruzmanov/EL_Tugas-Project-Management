be6a2deaf2c2d7bf506d93f1685a776187819f0c
Elnur-web <elnurruzmanov219autp@gmail.com>
2021-06-04 19:21:38 +0500

    addd

---
+++ main.js
@@ -0,0 +1,23 @@
+// let myChart = document.getElementById("myChart").getcontext('2d')
+
+
+
+// let massPopChart = new Chart(myChart,{
+//     type:'bar',
+//     data:{
+//         labels:['Tashkent', 'Karshi', 'Bukhara', 'Samarkand', 'Shakhrisabz'],
+//         datasets:[{
+//             label:'Population',
+//             data:[
+//                617594,
+//                181045, 
+//                153060,
+//                106519,
+//                105162,
+//                95072 
+//             ],
+//             bacgroundColor:"Green "
+//         }]
+//     }, 
+//     options:{}
+// })
